258407c9e2ef17eb66f79af2c7dc120e923b480c
Haroon <haroon20@hotmail.co.uk>
2022-01-17 16:39:08 +0000

    Step 4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,6 @@
-#require 'fizzbuzz' - Same as Line 3
-#require './lib/fizzbuzz'- Same as Line 3
-require_relative '../lib/fizzbuzz'
+# require './lib/fizzbuzz'- Same as Line 3
+# require_relative '../lib/fizzbuzz' - Same as Line 3
+require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
